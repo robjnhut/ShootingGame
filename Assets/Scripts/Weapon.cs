af60e6b21711c0cedb438871e5489f879c4b3dcb
Nobi Nobidi <nguyenhuudinh15102@gmail.com>
2024-10-01 01:17:50 +0700

    update new version on code for shoting game

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,41 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bulletPrefabs;
     public Transform bulletSpawn;
-    public float bulletVelocity = 30f;
+    public float bulletVelocity = 30;
     public float bulletPrefabsLifeTime = 3f;
     // Update is called once per frame
     void Update()
     {
+        //left mouse click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
         
+    }
+
+    private void FireWeapon()
+    {
+        // Kiểm tra vị trí bulletSpawn
+        Debug.Log("Bullet Spawn Position: " + bulletSpawn.position);
+        Debug.Log(bulletSpawn.forward);
+
+
+        // instantiate the bullet
+        GameObject bullet =  Instantiate(bulletPrefabs, bulletSpawn.position, Quaternion.identity);
+        //shoot the bullet
+        
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized *  bulletVelocity, ForceMode.Impulse);
+        //destroy the bullet after a few seconds
+        StartCoroutine(DestroyBulletAfterTime(bullet,bulletPrefabsLifeTime));
+
+
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+
     }
 }
